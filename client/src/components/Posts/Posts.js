c244844410cb1a4f3a1ad7b1a97d7680d3e154dc
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-31 13:58:18 +0200

    Using redux and making creating a memory form

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Post } from './Post/Post';
+import { useSelector } from 'react-redux';
 
+import { Post } from './Post/Post';
 import useStyles from './styles';
 
 export const Posts = () => {
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
+
+  console.log(posts);
   return (
     <>
       <h1>Posts</h1>
--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://borislavbenderov:database123@cluster0.hmzlcqi.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
